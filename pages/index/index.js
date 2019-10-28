74bc404694ef0694cad5105b23bdebef9c10df8a
xingway <876657294@qq.com>
2019-10-28 08:05:23 +0800

    fixed: portrait mode support

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,165 +1,122 @@
-import * as THREE from '../../libs/vendors/three'
-import * as THREEAR from '../../libs/vendors/THREEAR'
+const THREE = require('../../libs/three')
+const THREEAR  = require('../../libs/THREEAR')
 
-let camera, scene, renderer, canvas
-let id, torus, markerGroup
-let frame, controller, lastTime = 0
-let inited = false
 let w, h
-let frameSliceIndex
-
-const app = getApp()
-const RATIO = 4/3
+let canvas, frame
+let scene, camera, renderer
+let cube, torus, markerGroup
+let controller
+const RATIO = 0.5625
+const CANVAS_WIDTH = 480, CANVAS_HEIGHT = 640
+const FRAME_H = CANVAS_WIDTH / RATIO | 0
 
 Page({
 
-  data: {
-    canvasStyle: '',
-    cameraStyle: ''
-  },
-  
-  initStyles() {
-    const info = wx.getSystemInfoSync()
-    w = info.windowWidth
-    h = w / RATIO | 0
-    const style = `width:${w}px;height:${h}px;`
-    this.setData({
-      canvasStyle: style,
-      cameraStyle: style
-    })
-  },
-
-  initCamera() {
-    const info = wx.getSystemInfoSync()
-    const context = wx.createCameraContext()
-    const listener = context.onCameraFrame((_frame) => {
-      frame = _frame
-      if (!inited) {
-        this.initScene()
-        inited = true
-      }
-    })
-    listener.start()
-  },
-
-  debug() {
-    const info = wx.getSystemInfoSync()
-    const tw = info.windowWidth
-    const th = info.windowHeight
-    const sw = info.screenWidth
-    const sh = info.screenHeight
-
-    wx.showModal({
-      title: '提示',
-      content: `w: ${tw} h: ${th}`
-    })
-  },
-
-  initScene() {
-
-    const vw = frame.width
-    const vh = vw / RATIO | 0
-
-    renderer = new THREE.WebGLRenderer({
-      alpha: true,
-      canvas,
-      antialias: false
-    })
-
-    renderer.setSize(w, h)
-
+  _initScene() {
     scene = new THREE.Scene()
     camera = new THREE.Camera()
-    scene.add(camera)
+    renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+      canvas
+    })
+    renderer.setSize(CANVAS_WIDTH, FRAME_H)
 
     markerGroup = new THREE.Group()
     scene.add(markerGroup)
 
-    const source = new THREEAR.Source({ 
-      renderer, 
-      camera
-    })
-
-    THREEAR.initialize({ 
-      source, 
-      canvasWidth: vw,
-      canvasHeight: vh
-    }).then((_controller) => {
-      controller = _controller
-      this.initialize(_controller)
-      this.render()
-    })
-
-    frameSliceIndex = vw * (frame.height - vh) * 4
-    
-  },
-
-  initialize(controller) {
     const torusGeo = new THREE.TorusKnotGeometry(0.3, 0.1, 64, 16)
     const torusMat = new THREE.MeshNormalMaterial()
     torus = new THREE.Mesh(torusGeo, torusMat)
     torus.position.y = 0.5
-
-    /*
-    const axesHelper = new THREE.AxesHelper(5)
-    markerGroup.add(axesHelper)
-    */
-
     markerGroup.add(torus)
+
     const cubeGeo = new THREE.CubeGeometry(1, 1, 1)
     const cubeMat = new THREE.MeshNormalMaterial({
-      transparent : true,
+      transparent: true,
       opacity: 0.5,
       side: THREE.DoubleSide
     })
-    const cube = new THREE.Mesh(cubeGeo, cubeMat)
-    cube.position.y	= cubeGeo.parameters.height / 2
+    cube = new THREE.Mesh(cubeGeo, cubeMat)
+    cube.position.y = 0.5
     markerGroup.add(cube)
 
-    const patternMarker = new THREEAR.PatternMarker({
-      patternUrl: 'https://www.xingway.com/ar/data/patt.hiro',
-      markerObject: markerGroup
+    const source = new THREEAR.Source({
+      renderer, camera
     })
-    controller.trackMarker(patternMarker)
+    THREEAR.initialize({
+      source,
+      width: CANVAS_WIDTH,
+      height: FRAME_H
+    }).then((_controller) => {
+      const patternMarker = new THREEAR.PatternMarker({
+        patternUrl: 'https://www.xingway.com/ar/data/patt.hiro',
+        markerObject: markerGroup
+      })
+      _controller.trackMarker(patternMarker)
+      controller = _controller
+      this._render()
+    })
   },
 
-  render(now) {
-    if (frame) {
-      lastTime = lastTime || now - 1000 / 60
-      const delta = Math.min(200, now - lastTime)
-      lastTime = now
-      let rawData = new Uint8Array(frame.data)
-      rawData = this.sliceData(rawData)
-      controller.update(rawData)
-      torus.rotation.y += delta / 1000 * Math.PI
-      torus.rotation.z += delta / 1000 * Math.PI
-      renderer.render(scene, camera)
-    }
-    id = canvas.requestAnimationFrame(this.render.bind(this))
+  _initCamera() {
+    const context = wx.createCameraContext()
+    const listener = context.onCameraFrame((_frame) => {
+      if (!frame) {
+        this._initScene()
+        //this._log(_frame.width + ': ' + _frame.height)
+      }
+      frame = _frame
+    })
+    listener.start()
   },
 
-  sliceData(rawData) {
-    return rawData.slice(frameSliceIndex)
+  _render() {
+    
+    const data = new Uint8Array(frame.data)
+    //const rawData = this._buildData(data)
+    controller.update(data)
+    torus.rotation.y += 0.01
+    torus.rotation.z += 0.01
+    renderer.render(scene, camera)
+    canvas.requestAnimationFrame(this._render.bind(this))
   },
 
-  initCanvas() {
+  _buildData(data) {
+    const num = (FRAME_H - CANVAS_HEIGHT) * 4
+    const arr = new Uint8Array(num)
+    return new Uint8Array([...data, ...arr])
+  },
+
+  _init() {
     const query = wx.createSelectorQuery()
-    query.select('#webgl').node().exec((res) => {
+    query.select('#canvas').node().exec((res) => {
       canvas = res[0].node
-      this.initStyles()
-      this.initCamera()
+      this._initCamera()
+    })
+  },
+
+  _resize() {
+    const info = wx.getSystemInfoSync()
+    w = info.windowWidth
+    h = info.windowHeight
+    // this.setData({
+    //   cameraStyle: `width:${w}px;height:${w/RATIO|0}px;`,
+    //   webglStyle: `width:${w}px;height:${w/0.75|0}px;`
+    // })
+    // console.log(this.data)
+  },
+
+  _log(msg) {
+    wx.showModal({
+      title: '提示',
+      content: msg
     })
   },
 
   onLoad() {
-    wx.setNavigationBarTitle({
-      title: '小程序AR测试'
-    })
-    this.initCanvas()
-  },
-
-  onUnload() {
-    canvas.cancelAnimationFrame(id)
+    this._resize()
+    this._init()
   }
 
 })
